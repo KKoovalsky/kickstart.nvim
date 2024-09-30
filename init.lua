6bc242bb81e2978a588b3c9034ea6a5a98b0db3b
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2024-09-30 14:50:30 +0200

    Add vim grepper

--- init.lua
+++ init.lua
@@ -941,7 +941,9 @@ require('lazy').setup({
   {
     'preservim/tagbar',
   },
-
+  {
+    'mhinz/vim-grepper',
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
