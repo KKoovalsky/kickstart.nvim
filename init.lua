a5cd9749581b11d3bb63b3c73859336e9a1c041c
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2025-05-08 10:32:49 +0200

    Use python formatters

--- init.lua
+++ init.lua
@@ -671,6 +671,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        python = { 'black', 'isort' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
