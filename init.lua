b49b31084c6751725d665dfb0a8276009181719f
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2025-09-19 11:54:49 +0200

    Add clang-format

--- init.lua
+++ init.lua
@@ -963,6 +963,9 @@ require('lazy').setup({
       require('claude-code').setup()
     end,
   },
+  {
+    'rhysd/vim-clang-format',
+  },
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
