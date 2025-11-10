640bd545a5b5567438b9bcdf400179a1654f8764
Kacper Kowalski <kacper.s.kowalski@gmail.com>
2025-11-10 14:47:17 +0100

    configure todo-comments with no luck

--- init.lua
+++ init.lua
@@ -825,7 +825,22 @@ require('lazy').setup({
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = false,
+      keywords = {
+        TODO = { icon = ' ', color = 'info', alt = { 'todo', 'TODO:' } },
+        NOTE = { icon = ' ', color = 'hint', alt = { 'note', 'NOTE:' } },
+      },
+      search = {
+        pattern = [[(\b(KEYWORDS):|@(KEYWORDS)\b)]],
+        -- pattern = [[\b(KEYWORDS)]], -- ripgrep regex
+      },
+    },
+  },
 
   { -- Collection of various small independent plugins/modules
     'echasnovski/mini.nvim',
